2634febebc993b74d09b371311108e5f6c7cb0e1
Fábio Junik <fabiojunik@gmail.com>
2022-04-11 20:57:17 +0100

    Criado o formulario para editar nota

---
+++ Explicacao/frmEditarNota.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Explicacao
+{
+    partial class frmEditarNota
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEditarNota));
+            this.pnlBarraTitulo = new System.Windows.Forms.Panel();
+            this.btnFechar = new System.Windows.Forms.PictureBox();
+            this.btnCancelar = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNota = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtProva = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.btnSalvar = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.pnlBarraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFechar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlBarraTitulo
+            // 
+            this.pnlBarraTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBarraTitulo.Controls.Add(this.btnFechar);
+            this.pnlBarraTitulo.Location = new System.Drawing.Point(7, 7);
+            this.pnlBarraTitulo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlBarraTitulo.Name = "pnlBarraTitulo";
+            this.pnlBarraTitulo.Size = new System.Drawing.Size(482, 30);
+            this.pnlBarraTitulo.TabIndex = 111;
+            this.pnlBarraTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlBarraTitulo_MouseDown);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFechar.Image = ((System.Drawing.Image)(resources.GetObject("btnFechar.Image")));
+            this.btnFechar.Location = new System.Drawing.Point(460, 3);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(18, 18);
+            this.btnFechar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnFechar.TabIndex = 1;
+            this.btnFechar.TabStop = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.CheckedState.Parent = this.btnCancelar;
+            this.btnCancelar.CustomImages.Parent = this.btnCancelar;
+            this.btnCancelar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(97)))), ((int)(((byte)(129)))));
+            this.btnCancelar.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.HoverState.Parent = this.btnCancelar;
+            this.btnCancelar.Location = new System.Drawing.Point(249, 384);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.ShadowDecoration.Parent = this.btnCancelar;
+            this.btnCancelar.Size = new System.Drawing.Size(163, 40);
+            this.btnCancelar.TabIndex = 121;
+            this.btnCancelar.Text = "Cancelar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 22);
+            this.label1.TabIndex = 117;
+            this.label1.Text = "Nota";
+            // 
+            // txtNota
+            // 
+            this.txtNota.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNota.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtNota.HintForeColor = System.Drawing.Color.Empty;
+            this.txtNota.HintText = "";
+            this.txtNota.isPassword = false;
+            this.txtNota.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txtNota.LineIdleColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(155)))), ((int)(((byte)(230)))));
+            this.txtNota.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txtNota.LineThickness = 1;
+            this.txtNota.Location = new System.Drawing.Point(64, 247);
+            this.txtNota.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNota.Name = "txtNota";
+            this.txtNota.Size = new System.Drawing.Size(348, 42);
+            this.txtNota.TabIndex = 116;
+            this.txtNota.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 16F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(121)))), ((int)(((byte)(126)))));
+            this.label3.Location = new System.Drawing.Point(59, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 27);
+            this.label3.TabIndex = 115;
+            this.label3.Text = "Editar Nota";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 22);
+            this.label7.TabIndex = 114;
+            this.label7.Text = "Prova";
+            // 
+            // txtProva
+            // 
+            this.txtProva.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtProva.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProva.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtProva.HintForeColor = System.Drawing.Color.Empty;
+            this.txtProva.HintText = "";
+            this.txtProva.isPassword = false;
+            this.txtProva.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txtProva.LineIdleColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(155)))), ((int)(((byte)(230)))));
+            this.txtProva.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txtProva.LineThickness = 1;
+            this.txtProva.Location = new System.Drawing.Point(64, 140);
+            this.txtProva.Margin = new System.Windows.Forms.Padding(4);
+            this.txtProva.Name = "txtProva";
+            this.txtProva.Size = new System.Drawing.Size(348, 42);
+            this.txtProva.TabIndex = 113;
+            this.txtProva.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.CheckedState.Parent = this.btnSalvar;
+            this.btnSalvar.CustomImages.Parent = this.btnSalvar;
+            this.btnSalvar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(230)))), ((int)(((byte)(119)))));
+            this.btnSalvar.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.HoverState.Parent = this.btnSalvar;
+            this.btnSalvar.Location = new System.Drawing.Point(64, 384);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.ShadowDecoration.Parent = this.btnSalvar;
+            this.btnSalvar.Size = new System.Drawing.Size(163, 40);
+            this.btnSalvar.TabIndex = 112;
+            this.btnSalvar.Text = "Salvar";
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.BorderColor = System.Drawing.Color.Silver;
+            this.guna2Button1.BorderThickness = 1;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Enabled = false;
+            this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(1, 1);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.PressedColor = System.Drawing.Color.Transparent;
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(492, 466);
+            this.guna2Button1.TabIndex = 162;
+            // 
+            // frmEditarNota
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(241)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(494, 468);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNota);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtProva);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.pnlBarraTitulo);
+            this.Controls.Add(this.guna2Button1);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmEditarNota";
+            this.Text = "frmEditarNota";
+            this.pnlBarraTitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnFechar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBarraTitulo;
+        private System.Windows.Forms.PictureBox btnFechar;
+        private Guna.UI2.WinForms.Guna2Button btnCancelar;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txtNota;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txtProva;
+        private Guna.UI2.WinForms.Guna2Button btnSalvar;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
---
+++ Explicacao/frmEditarNota.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Explicacao
+{
+    public partial class frmEditarNota : Form
+    {
+        Panel painel;
+        int codAluno;
+        int codTurma;
+
+        public frmEditarNota(Panel pnl, int codAluno, int codTurma)
+        {
+            InitializeComponent();
+            painel = pnl;
+            this.codAluno = codAluno;
+            this.codTurma = codTurma;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void pnlBarraTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
