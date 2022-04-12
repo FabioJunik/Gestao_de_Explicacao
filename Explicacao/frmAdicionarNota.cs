a9d5a8d6ce299de44113e6cae562736808f5ac77
Fábio Junik <fabiojunik@gmail.com>
2022-04-12 14:23:00 +0100

    Mostrando o form do adicionar nota fora do painel

---
+++ Explicacao/frmAdicionarNota.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using MySql.Data.MySqlClient;
+
+namespace Explicacao
+{
+    public partial class frmAdicionarNota : Form
+    {
+
+        int codAluno;
+        int codTurma;
+
+        public frmAdicionarNota(int codAluno, int codTurma)
+        {
+            InitializeComponent();
+            this.codAluno = codAluno;
+            this.codTurma = codTurma;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pnlBarraTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
